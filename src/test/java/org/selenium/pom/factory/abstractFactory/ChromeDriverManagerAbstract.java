7f8890c62fc0edc3d34a477ce48ef6fc2ef36d22
Naresh Babu Dasarapu <nareshbabu012@gmail.com>
2025-03-17 16:52:09 +0530

    enabled headless mode for chrome

--- src/test/java/org/selenium/pom/factory/abstractFactory/ChromeDriverManagerAbstract.java
+++ src/test/java/org/selenium/pom/factory/abstractFactory/ChromeDriverManagerAbstract.java
@@ -3,6 +3,7 @@ package org.selenium.pom.factory.abstractFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.selenium.pom.factory.DriverManager;
 
 public class ChromeDriverManagerAbstract extends DriverManagerAbstract {
@@ -10,7 +11,9 @@ public class ChromeDriverManagerAbstract extends DriverManagerAbstract {
     @Override
     public void startDriver() {
         WebDriverManager.chromedriver().cachePath("Drivers").setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 }
